35556149f24311ff463313ee4759f244e99d2615
anix001 <chaudharyanish305@gmail.com>
2024-09-08 12:41:43 +0545

    Improvement: Error message

--- src/user.ts
+++ src/user.ts
@@ -6,6 +6,6 @@ export default async function getUserData(userId:number) {
     const data = await fetchData(url);
     return data;
     } catch(error:any){
-        throw new Error(`${error.message}: Unable to fetch user data`);
+        throw new Error(`${error.message}: Unable to fetch user data!`);
     }
 }
